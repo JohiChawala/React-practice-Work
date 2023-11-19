153d7726d75983688d681e8bd86002a5c23aee5e
JohiChawala <136928760+JohiChawala@users.noreply.github.com>
2023-11-19 22:03:09 +0500

    static react page

---
+++ Scrimba React work/React first Static page/index.js
@@ -0,0 +1,13 @@
+// Spend a minute memorizing the line below
+ReactDOM.render(<h1>Hello, everyone!</h1>, document.getElementById("root"))
+ReactDOM.render(<h1>juhi chawal here </h1>, document.getElementById("root"))
+ReactDOM.render(<h1>juhi queen here </h1>, document.querySelector("#root"))
+
+ReactDOM.render(
+    <ol>
+        <li>bye</li>
+        <li>good</li>
+    </ol>
+    ,
+    document.querySelector("#root1")
+)
